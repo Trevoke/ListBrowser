b1095736e2aed4e0d17b5daa54d906b904d705dd
Aldric Giacomoni <aldric@trevoke.net>
2010-01-15 18:51:11 -0500

    First gem release.

--- bin/list_browser.rb
+++ bin/list_browser.rb
@@ -1,3 +1,13 @@
+# A class to browse tree structures.
+# When creating the object, you have one required attribute and three optional
+# attributes:
+# node (the node where you wish to start browsing, usually the root of the tree)
+#
+# parent (the name of the method used to get to the parent)
+#
+# children (the name of the method used to get to the children)
+#
+# properties (the name of the method used to get to properties)
 class ListBrowser
 
   attr_accessor :node, :parent, :children, :properties
@@ -11,12 +21,16 @@ class ListBrowser
     generate_menu
   end
 
+  # Call this method to go to the current node's parent
   def parent
     unless @node.send(@parent).nil?
       @node = @node.send(@parent)
     end
   end
 
+  # Call this method, with a number between 0 and the size of the array of
+  # children -1 (if there are three children, between 0 and 2), to go to
+  # the current node's child.
   def child number
     unless @node.send(@children).nil?
       @node = ( ( @node.send(@children) ).at(number) )
@@ -53,6 +67,15 @@ class ListBrowser
         else
           child input.to_i - 1
       end
+    end
+  end
+
+  def print_properties
+    properties = @node.send(@properties)
+    keylength = properties.max {|a, b| a.size }
+    #valuelength = properties.max {|a, b| b.size}
+    properties.each do |k, v|
+      puts "#{k.ljust(keylength)} => #{v}"
     end
   end
 
